e836e02463bd7f6a7717fb57cc1ab5a8c0caa4bf
OleksandrVaniuta <vaniutasa@gmail.com>
2023-02-28 10:52:48 +0200

    hero slider

--- src/js/hero-slider.js
+++ src/js/hero-slider.js
@@ -1,6 +1,45 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import Swiper, {
+  Navigation,
+  Keyboard,
+  Pagination,
+  EffectFade,
+  Autoplay,
+} from 'swiper';
 
-new Swiper('.hero-swiper');
+import '../../node_modules/swiper/swiper.scss';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination, Keyboard, Navigation, EffectFade, Autoplay],
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  //   mousewheel: {
+  //     invert: true,
+  //   },
+  //   spaceBetween: 180,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  //   8
+  spaceBetween: 400,
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  autoHeight: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  // pauseOnMouseEnter: true,
+});
